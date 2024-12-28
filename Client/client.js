79484ad2700bc11209a5ab769aa404fe2f9ae288
V1Azam <v1.0azam@gmail.com>
2024-12-28 17:34:26 +0000

    added functionality to register route as well as a successful login

--- Client/client.js
+++ Client/client.js
@@ -1,10 +1,9 @@
 const nameInput = document.getElementById("login");
+let regInstead = document.getElementById('studentNotFound')
 nameInput.addEventListener("submit", async function (event) {
     event.preventDefault();
-    let regInstead = document.getElementById('studentNotFound')
     let nameInputData = new FormData(nameInput)
     let nameInputJSON = JSON.stringify(Object.fromEntries(nameInputData.entries()));
-    console.log(nameInputJSON)
     let response = await fetch('/checkExistingStudent',
         {
             method: 'Post',
@@ -14,10 +13,19 @@ nameInput.addEventListener("submit", async function (event) {
             body: nameInputJSON
         });
     let responseData = await response.json();
-    console.log(responseData.exists);
     if (responseData.exists) {
+        console.log(regInstead.style.display);
         regInstead.style.display = "none";
+        document.getElementById('loginStudent').style.display = "none";
+        document.getElementById('login').style.display = "none";
     } else {
         regInstead.style.display = "block";
     }
 });
+
+document.getElementById("registerLink").onclick = function (event) {
+    event.preventDefault();
+    document.getElementById('registerNewStudent').style.display = "block";
+    document.getElementById('login').style.display = "none"
+    regInstead.style.display = "none";
+}
