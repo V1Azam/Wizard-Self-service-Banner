3253214dfa6d843ffa27152444d00474313ddfdf
V1Azam <v1.0azam@gmail.com>
2024-12-30 07:10:08 +0000

    fixed centre of successful login

--- Client/client.js
+++ Client/client.js
@@ -1,5 +1,19 @@
 const nameInput = document.getElementById("login");
 let regInstead = document.getElementById('studentNotFound')
+
+function setUpStudentPage(obj) {
+    regInstead.style.display = "none";
+    document.getElementById('loginStudent').style.display = "none";
+    document.getElementById('successfulLogin').style.display = "block";
+    document.getElementById('loggedInStudent').innerHTML = obj.name;
+    if (obj.gender == "Male") {
+        document.getElementById('genderPic').src = "./assets/img/male.png";
+    } else {
+        document.getElementById('genderPic').src = "./assets/img/female.png";
+    }
+    document.getElementById('bio').innerHTML = ("You like " + obj.favSweet + "!");
+}
+
 nameInput.addEventListener("submit", async function (event) {
     event.preventDefault();
     let nameInputData = new FormData(nameInput)
@@ -14,11 +28,7 @@ nameInput.addEventListener("submit", async function (event) {
         });
     let responseData = await response.json();
     if (responseData.name) {
-        regInstead.style.display = "none";
-        document.getElementById('loginStudent').style.display = "none";
-        document.getElementById('login').style.display = "none";
-        document.getElementById('successfulLogin').style.display = "block";
-        document.getElementById('loggedInStudent').innerHTML = responseData.name;
+        setUpStudentPage(responseData);
     } else {
         regInstead.style.display = "block";
     }
