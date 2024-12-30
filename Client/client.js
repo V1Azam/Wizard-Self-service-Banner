afb3dc84fe937c6142946b0af8d8690fbed186cd
V1Azam <v1.0azam@gmail.com>
2024-12-30 21:54:35 +0000

    opened tab div for scroll box

--- Client/client.js
+++ Client/client.js
@@ -1,17 +1,24 @@
 const nameInput = document.getElementById("login");
-let regInstead = document.getElementById('studentNotFound')
+
+function visual(elem, act) {
+    document.getElementById(elem).style.display = act;
+}
 
 function setUpStudentPage(obj) {
-    regInstead.style.display = "none";
+    visual('studentNotFound', "none");
     document.getElementById('loginStudent').style.display = "none";
     document.getElementById('successfulLogin').style.display = "block";
     document.getElementById('loggedInStudent').innerHTML = obj.name;
+
     if (obj.gender == "Male") {
         document.getElementById('genderPic').src = "./assets/img/male.png";
     } else {
         document.getElementById('genderPic').src = "./assets/img/female.png";
     }
     document.getElementById('bio').innerHTML = ("You like " + obj.favSweet + "!");
+
+    document.getElementById('scrollTitleMastery').innerHTML = (obj.specialisation + " students of 2025");
+    document.getElementById('scrollTitleHouse').innerHTML = (obj.house + " students of 2025");
 }
 
 function tab(event, tabName) {
@@ -48,7 +55,7 @@ nameInput.addEventListener("submit", async function (event) {
     if (responseData.name) {
         setUpStudentPage(responseData);
     } else {
-        regInstead.style.display = "block";
+        visual('studentNotFound', "block");
     }
 });
 
@@ -56,7 +63,7 @@ document.getElementById("registerLink").onclick = function (event) {
     event.preventDefault();
     document.getElementById('registerNewStudent').style.display = "block";
     document.getElementById('login').style.display = "none"
-    regInstead.style.display = "none";
+    visual('studentNotFound', "none");
 }
 
 // Set default tab to London on page load
